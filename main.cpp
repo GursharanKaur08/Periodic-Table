5f24eb6f984b677d6b06418f78bec2226f1360ec
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-22 02:19:24 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,26 +3,31 @@
 //**********************************************************
 
 void frame()
-	{	 int i,j;
+	{	 
+		int i,j;
  		clrscr();
  		gotoxy(33,2); cout<<"PERIODIC TABLE";
  		gotoxy(1,1); cout<<"É";
  		
 		for(i=2;i<=79;i++)
- 		{	  gotoxy(i,1); cout<<"Í";
+ 		{	  
+			gotoxy(i,1); cout<<"Í";
  		}
  		cout<<"»";
  		
 		for(i=2;i<=23;i++)
- 		{	  gotoxy(80,i); cout<<"º";
+ 		{	  
+			gotoxy(80,i); cout<<"º";
  		}
  		gotoxy(80,24); cout<<"¼";
  
 		for(i=2;i<=79;i++)
- 		{	  gotoxy(i,24); cout<<"Í";
+ 		{	  
+			gotoxy(i,24); cout<<"Í";
  		} 
 		
 		for(i=2;i<=23;i++)
- 		{	  gotoxy(1,i); cout<<"º";
-		 }
+ 		{	  
+			gotoxy(1,i); cout<<"º";
+		}
 	}
